8cf49f254d3a543e0ce70daf2049cebf9653fbf5
pjc02478 <pjc0247@gmail.com>
2016-09-12 16:14:49 +0900

    d

---
+++ src/DefinitelyOne/DefinitelyOne/Yahoo/YQL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsRestClient;
+using CsRestClient.Utility;
+using CsRestClient.Attributes.Request;
+using CsRestClient.Attributes.Response;
+
+namespace DefinitelyOne.Yahoo
+{
+    [Service("v1/public")]
+    public interface YQLInterface
+    {
+        [Resource("yql?q={0}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
+        Task<string> Query([Binding]string query);
+    }
+
+    public class YQL
+    {
+        private static YQLInterface impl
+        {
+            get
+            {
+                return RemotePoint.Create<YQLInterface>("https://query.yahooapis.com");
+            }
+        }
+
+        public static Task<string> Query(string query)
+        {
+            return impl.Query(query);
+        }
+
+        public static async Task<float> GetCurrencyRate(string code1, string code2)
+        {
+            string json = await impl.Query(
+                $"select * from yahoo.finance.xchange where pair in (\"{code1 + code2}\")");
+            return JsonPathParser.ParseObject<float>(json, "query.results.rate.Rate");
+        }
+        public static async Task<CurrencyInfo> GetCurrencyInfo(string code1, string code2)
+        {
+            string json = await impl.Query(
+                $"select * from yahoo.finance.xchange where pair in (\"{code1 + code2}\")");
+            return JsonPathParser.ParseObject<CurrencyInfo>(json, "query.results.rate");
+        }
+    }
+
+    public class CurrencyInfo
+    {
+        public string Name { get; set; }
+        public float Rate { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public float Ask { get; set; }
+        public float Bid { get; set; }
+    }
+}
