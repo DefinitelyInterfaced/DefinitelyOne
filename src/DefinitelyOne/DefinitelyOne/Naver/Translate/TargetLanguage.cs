a6a593ff10d6628b9490c10fb97d162cfaffd2d7
pjc02478 <pjc0247@gmail.com>
2016-09-12 16:17:07 +0900

    f

---
+++ src/DefinitelyOne/DefinitelyOne/Naver/Translate/TargetLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefinitelyOne.Naver.Translate
+{
+    public class TargetLanguage
+    {
+        public static readonly string Korean = "ko";
+        public static readonly string English = "en";
+        public static readonly string Chinese = "zh-CN";
+        public static readonly string Japanese = "ja";
+    }
+}
---
+++ src/DefinitelyOne/DefinitelyOne/Naver/Translate/Translate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsRestClient;
+using CsRestClient.Pipeline;
+using CsRestClient.Attributes;
+using CsRestClient.Attributes.Request;
+using CsRestClient.Attributes.Response;
+
+namespace DefinitelyOne.Naver.Translate
+{
+    [Service("v1/language")]
+    public interface TranslateAPIInterface : WithCommonHeader
+    {
+        [Post, Resource("translate")]
+        [JsonPath("message.result.translatedText")]
+        Task<string> Translate([RequestUri]string source, [RequestUri]string target, [RequestUri]string text);
+    }
+
+    public class TranslateAPI
+    {
+        public static TranslateAPIInterface Create(string clientId, string clientSecret)
+        {
+            var api = RemotePoint.Create<TranslateAPIInterface>("https://openapi.naver.com");
+
+            api.commonHeaders = new Dictionary<string, string>()
+            {
+                ["X-Naver-Client-Id"] = clientId,
+                ["X-Naver-Client-Secret"] = clientSecret
+            };
+
+            return api;
+        }
+    }
+}
